c5a239d32e2c526e4da9dab517055ce338214296
Diarmuid Collins <dcollins@curtisswright.com>
2014-03-19 11:41:06 +0000

    Fixed call to the init

--- KadMat101/Mat101GPIO.py
+++ KadMat101/Mat101GPIO.py
@@ -7,7 +7,7 @@ class Mat101GPIO():
         self.libmat101 = CDLL("libmat101.so.1")
 
         try:
-            self.libmat101.mat101_init
+            self.libmat101.mat101_init()
         except:
             raise Exception
 
