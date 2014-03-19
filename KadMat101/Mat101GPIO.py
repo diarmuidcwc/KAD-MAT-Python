2c1b1509afc673492b8f12e11553c8ee90ea7532
Diarmuid Collins <dcollins@curtisswright.com>
2014-03-19 09:28:09 +0000

    Added the guts of the GPIO

--- KadMat101/Mat101GPIO.py
+++ KadMat101/Mat101GPIO.py
@@ -2,9 +2,6 @@ from ctypes import *
 
 class Mat101GPIO(self):
 
-    GPIO_DIR_OUTPUT = 0
-    GPIO_DIR_INPUT = 1
-
     def __init__(self):
         cdll.LoadLibrary("libmat101.so.1")
         self.libmat101 = CDLL("libmat101.so.1")
@@ -14,5 +11,24 @@ class Mat101GPIO(self):
         except:
             raise Exception
 
-        self.Bank1Direction = "out"
-        self.Bank2Direction = "out"
+        self.BankDirection = ["OUT","OUT"]
+
+
+    def ConfigureBankDirection(self):
+        DirToHW = {"OUT" : 0, "IN" : 1}
+        try:
+            self.libmat101.mat101_gpio_setDir(DirToHW[self.BankDirection[0]],DirToHW[self.BankDirection[1]])
+        except:
+            raise Exception("Failed to set BankDirection ")
+
+
+    def WriteToGPIO(self,data):
+        try:
+            data_char = c_ubyte(data)
+        except:
+            raise Exception ("Cannot convert input to 8 bit character")
+        self.libmat101.mat101_gpio_set(data_char)
+
+
+    def CloseGPIO(self):
+        self.libmat101.mat101_exit()
